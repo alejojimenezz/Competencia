e34a6c8342b0568b21cfc479d21520c32e5c2086
Estudiante <Estudiante@DESKTOP-KS3QO24>
2017-03-29 16:19:46 -0500

    Merge origin/master
    Conflicts:
    	src/ejerciciopoo/POO.java

--- src/Competencia/Main.java
+++ src/Competencia/Main.java
@@ -11,8 +11,18 @@ import ch.aplu.turtle.Turtle;
  */
 public class Main {
     
-    double r1 = 25;
-    double l = 40;
-    double r2 = 12.5;
-    Cuadrado c = new Cuadrado (0, 0, 0, l);
+    public static void main(String[] args) {
+        double r1 = 25;
+        int l = 40;
+        double r2 = 12.5;
+        Circulo [] c1 = new Circulo [8];
+        for (int i = 0; i < 6; i++) {
+            c1[i]= new Circulo (25,0,-60+(20*i));
+            c1[i].dibujar25();
+        }
+        Cuadrado c = new Cuadrado ();
+        c.dibujar(0, 0, 0, l);
+        Elipse e = new Elipse ();
+    }
+    
 }
