06057ce515643d9a11b3a785ff6812160f7927e2
Farid Cortes <difcortesgu@unal.edu.co>
2017-03-29 15:58:19 -0500

    Add files via upload

--- src/ejerciciopoo/Elipse.java
+++ src/ejerciciopoo/Elipse.java
@@ -10,35 +10,55 @@ public class Elipse {
         this.joe = joe;
     }
 
-    public void dibujarE(double x,double y,double a){
+    public void dibujarE(double x,double y,double a,boolean dir){
         joe.setPos(x,y);
         joe.setH(a);
         for(int j=0;j<2;j++){
             for(int z=0;z<15;z++){
+            if(dir){
                 joe.right(3);
+            }else{
+                joe.left(3);
+            }    
                 joe.forward(1);
             }
             for(int i=0;i<30;i++){
+            if(dir){
                 joe.right(3);
+            }else{
+                joe.left(3);
+            }    
                 joe.forward(3);
             } 
             for(int z=0;z<15;z++){
+            if(dir){
                 joe.right(3);
+            }else{
+                joe.left(3);
+            }    
                 joe.forward(1);
             }
             joe.setH(a+180);
         }
     }
     
-    public void dibujarA(double x,double y,double a){
+    public void dibujarA(double x,double y,double a,boolean dir){
         joe.setPos(x,y);
         joe.setH(a);
         for(int z=0;z<15;z++){
+            if(dir){
                 joe.right(3);
+            }else{
+                joe.left(3);
+            }    
                 joe.forward(1);
             }
             for(int i=0;i<15;i++){
+            if(dir){
                 joe.right(3);
+            }else{
+                joe.left(3);
+            }    
                 joe.forward(3);
             } 
     }
