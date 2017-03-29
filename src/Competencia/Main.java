a5d0ad7b513cbc7844f564f5c138733a6ce352f1
Estudiante <Estudiante@DESKTOP-KS3QO24>
2017-03-29 16:01:16 -0500

    Merge origin/master
    Conflicts:
    	src/ejerciciopoo/POO.java

---
+++ src/Competencia/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Competencia;
+import ch.aplu.turtle.Turtle;
+/**
+ *
+ * @author Estudiante
+ */
+public class Main {
+    
+    double r1 = 25;
+    double l = 40;
+    double r2 = 12.5;
+    Cuadrado c = new Cuadrado (0, 0, 0, l);
+}
